87456f931eee49f5ebebe5a45f1bdae6818d501f
roemellomccoy <roemello.mccoy22@gmail.com>
2021-06-22 14:17:06 -0500

    First commit

--- server.js
+++ server.js
@@ -1,3 +1,13 @@
 const http = require("http");
 
 // YOUR NODE SERVER CODE GOES HERE
+const server = http.createServer((request, response) => {
+  response.end("Hello Galvanize")
+});
+
+const HOST = 3000;
+const PORT = "127.0.0.1";
+
+  server.listen(3000, "127.0.0.1", () => {
+    console.log(`Server running at ${HOST}:${PORT}`);
+  });
